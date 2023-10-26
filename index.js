fbc74184ff78006a06d1897677e6d4043d64f296
michael <michaelyoussef396@gmail.com>
2023-10-26 20:54:56 +1100

    finished

---
+++ index.js
@@ -0,0 +1,17 @@
+function receivesAFunction(spy) {
+    spy()
+}
+
+function returnsANamedFunction() {
+    function namedFunction() {
+        console.log("This is a named function.");
+    }
+
+    return namedFunction;
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {
+        console.log("This is an anonymous function.");
+    };
+}
